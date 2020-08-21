4fd5dd1d92223304d02ca34e413cd51e5ecefa4c
Max Vuong <xuyenvuong@yahoo.com>
2020-08-20 21:41:41 -0700

    Setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -48,9 +48,9 @@ function uninstall_package() {
 	sudo apt purge -y $dpkg_name
   fi
 }
-
+#-------------------------------------------------------------------------------------------#
 # Main function to install all necessary package to support the node
-function install_all() {
+function install_essential() {
   # Update & Upgrade to latest
   sudo apt-get update && sudo apt-get upgrade
   
@@ -70,6 +70,35 @@ function install_all() {
   install_package python3-pip
 }
 
+# Install Python
+function install_python() { 
+  if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ]
+  then
+    wget -O /tmp/python.tgz https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
+    mkdir -p /tmp/Python
+    tar -C /tmp/Python --strip-components 1 -xvf /tmp/python.tgz
+	
+	cd /tmp/Python
+	
+	./configure --enable-optimizations
+	sudo make altinstall
+	
+	python3 --version
+	cd
+  fi
+}
+
+# Install Grafana
+function install_grafana() {
+  if [ $(dpkg-query -W -f='${Status}' grafana 2>/dev/null | grep -c "ok installed") -eq 0 ]
+  then
+    wget -O /tmp/grafana.deb https://dl.grafana.com/oss/release/grafana_7.0.3_arm64.deb
+	sudo dpkg --force-all -i /tmp/grafana.deb
+	rm /tmp/grafana.deb
+  fi 
+}
+
+#-------------------------------------------------------------------------------------------#
 # Purge all installed packages and their dependencies
 function uninstall_all() {
   # Uninstall docker
@@ -91,6 +120,18 @@ function uninstall_all() {
   sudo apt autoremove -y 
 }
 
+# Uninstall Python
+function uninstall_python() {
+  # TODO
+  echo "TODO: Uninstalling Python..."
+}
+
+# Uninstall Grafana
+function uninstall_grafana() {
+  sudo dpkg –-remove grafana
+}
+
+#-------------------------------------------------------------------------------------------#
 # Upgrade all
 function upgrade_all() {
   echo "Upgrading...."
@@ -99,7 +140,7 @@ function upgrade_all() {
   sudo apt-get update && sudo apt-get upgrade
   
   # Pull latest pi4-pos-setup.git repo
-  if [ -d $HOME/pos-setup ]
+  if [ ! -d $HOME/pos-setup ]
   then
     git clone https://github.com/xuyenvuong/pi4-pos-setup.git $HOME/pos-setup
   else
@@ -109,63 +150,109 @@ function upgrade_all() {
   fi  
 }
 
+#-------------------------------------------------------------------------------------------#
 # Initialize pos setup: important files/directories in order to run the PoS node
-function initialize_pos() {   
+function build_pos() {   
   # Define setup directories
-  mkdir -p $HOME/{.eth2,.eth2stats,.eth2validators,.ethereum,.password,logs,prysm/configs}
-  mkdir -p /etc/ethereum
-  mkdir -p /home/prometheus/node-exporter
+  mkdir -p $HOME/{.eth2,.eth2stats,.eth2validators,.ethereum,.password,logs,prysm,prysm/configs}
+  sudo mkdir -p /etc/ethereum
+  sudo mkdir -p /home/prometheus/node-exporter
   
   # Create files
   touch $HOME/.password/password.txt
   touch $HOME/logs/{beacon,validator,slasher}.log
   
   # Clone pi4-pos-setup.git repo
-  if [! -d $HOME/pos-setup ]
+  if [ ! -d $HOME/pos-setup ]
   then
     git clone https://github.com/xuyenvuong/pi4-pos-setup.git $HOME/pos-setup
   fi
   
   # Setup prysm script
-  curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output $HOME/prysm/prysm.sh && chmod +x $HOME/prysm/prysm.sh
+  curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output $HOME/prysm/prysm.sh && chmod +x $HOME/prysm/prysm.sh  
 }
 
+#-------------------------------------------------------------------------------------------#
 # Tear down files/directories used for PoS
 function teardown() {
   echo "Tearing down now..."
   # Remove setup directories
-  rm -r $HOME/{.eth2,.eth2stats,.eth2validators,.ethereum,.password,logs,prysm/configs}
-  rm -r /etc/ethereum
-  rm -r /home/prometheus
+  rm -rf $HOME/{.eth2,.eth2stats,.eth2validators,.ethereum,.password,logs,prysm/configs,prysm}
+  sudo rm -rf /etc/ethereum
+  sudo rm -rf /home/prometheus
   
   # Clone pos-setup repo
   rm -rf $HOME/pos-setup
 }
 
+#-------------------------------------------------------------------------------------------#
 # Run backup for all
 function backup_all() {
   echo "Backing up...."
 }
 
+#-------------------------------------------------------------------------------------------#
 # Verify setup 
 function verify() {
   echo "Verifying...."
 }
 
+#-------------------------------------------------------------------------------------------#
 # Display help
 function help() {
   echo "Help..."
 }
 
+#-------------------------------------------------------------------------------------------#
+function install_route() {
+  local opt=$1
+  
+  case $opt in
+    essential)
+	  echo "Essential ...."
+	  ;; #install_essential;;
+	python3)
+	  echo "Python3 ...."
+	  ;; #install_python;;
+	grafana)
+	  echo "Grafana ...."
+	  ;; #install_python;;
+	*)
+	  echo "Option '$1' is not found!"
+      echo "Please use 'setup.sh help' for more info."
+	  exit 1;;
+  esac
+}
+
+
+#-------------------------------------------------------------------------------------------#
 case $1 in
-  install) install_all;;
-  uninstall) uninstall_all;;
-  upgrade) upgrade_all;;
-  init) initialize_pos;;
-  teardown) teardown;; # Dev mode: Must remove when go live
-  backup) backup_all;;
-  verify) verify;;
-  help) help;;
+  -i|--install) 
+    install_route $2
+	;;  
+  uninstall)
+    uninstall_all
+	;;
+  upgrade)
+    upgrade_all
+	;;
+  build)
+    build_pos
+	;; 
+  teardown)
+    teardown
+	;; # Dev mode: Must remove when go live
+  
+  backup)
+    backup_all
+	;;
+  verify)
+    verify
+	;;
+  
+  help)
+    help
+	;;
   *)
     echo "Task '$1' is not found!"
     echo "Please use 'setup.sh help' for more info."
