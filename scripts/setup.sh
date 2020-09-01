6df8fa2b0ba3518e4de637110108505832bf4e6e
Max Vuong <xuyenvuong@yahoo.com>
2020-08-31 21:16:37 -0700

    Typo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -88,7 +88,7 @@ function install_docker() {
     sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-	# arch = amd64|amd64|armhf
+	# arch = amd64|arm64|armhf
     sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
