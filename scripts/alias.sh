05b6993c64b36397523cd86a1cee95cd98b344b0
Max Vuong <xuyenvuong@yahoo.com>
2022-04-28 17:57:15 -0700

    Update instruction

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -5,7 +5,7 @@
 
 : <<'COMMENT_BLOCK'
 Run this command to add aliases to your .bashrc
-curl -L https://raw.githubusercontent.com/xuyenvuong/pi4-pos-setup/master/scripts/alias.sh | bash | source $HOME/.bashrc
+> curl -L https://raw.githubusercontent.com/xuyenvuong/pi4-pos-setup/master/scripts/alias.sh | bash | source $HOME/.bashrc
 
 Description:
 Supporting services [beacon, validator, eth2-stats, geth, prometheus, prometheus-node-exporter, grafana]
@@ -14,7 +14,7 @@ Supporting post-fixes [-log, -start, -stop, -restart, -enable]
 Usage: Add post-fixes after your service name e.g. [service]-log.
 Example: "beacon-log", "validator-restart", "geth-stop', ect...
 
-Test by typing:
+Test, view beacon log by typing at the command prompt:
 > beacon-log
 
 COMMENT_BLOCK
