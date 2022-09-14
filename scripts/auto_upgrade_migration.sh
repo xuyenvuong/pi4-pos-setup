0524dea8082dcefa0f8ba22126da0e7a4b4dc4c6
Max Vuong <xuyenvuong@yahoo.com>
2022-09-13 22:23:45 -0700

    Create auto_upgrade_migration.sh
    Update and migrate to new auto_upgrade.sh script

---
+++ scripts/auto_upgrade_migration.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# auto_upgrade_migration.sh - A script to shorten the linux command for your convinient.
+# Author: Max Vuong
+# Date: 09/13/2022
+
+: <<'COMMENT_BLOCK'
+Run this command to update and migrate the auto_upgrade.sh script
+> curl -L https://raw.githubusercontent.com/xuyenvuong/pi4-pos-setup/master/scripts/auto_upgrade_migration.sh | bash
+
+COMMENT_BLOCK
+
+# ---------------------------------------------------------------
+
+cd ~
+DISCORD_WEBHOOK_URL=$(cat auto_upgrade.sh | grep 'https://discord.com/api/webhooks')
+GETH_PRUNE_AT_PERCENTAGE=$(cat auto_upgrade.sh | grep 'GETH_PRUNE_AT_PERCENTAGE=90')
+
+mv ~/auto_upgrade.sh auto_upgrade.sh.$(date "+%Y%m%d-%H%M%S")
+wget https://raw.githubusercontent.com/xuyenvuong/pi4-pos-setup/master/scripts/auto_upgrade.sh && chmod +x auto_upgrade.sh
+
+sed -i "s|^DISCORD_WEBHOOK_URL=''|$DISCORD_WEBHOOK_URL|g" ~/auto_upgrade.sh
+sed -i "s|^GETH_PRUNE_AT_PERCENTAGE=90|$GETH_PRUNE_AT_PERCENTAGE|g" ~/auto_upgrade.sh
+
+# ---------------------------------------------------------------
